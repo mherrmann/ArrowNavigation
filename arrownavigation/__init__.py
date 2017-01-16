7bb4a2c679af4dabf726a102721fc3e7be0168c6
Michael Herrmann <michael@herrmann.io>
2017-01-16 11:00:42 +0100

    Fix bug that completely broke the plugin

--- arrownavigation/__init__.py
+++ arrownavigation/__init__.py
@@ -1,7 +1,7 @@
 from fman import DirectoryPaneCommand
 from os.path import isdir, realpath
 
-class OpenDirectory(CorePaneCommand):
+class OpenDirectory(DirectoryPaneCommand):
 	def __call__(self):
 		file_under_cursor = self.pane.get_file_under_cursor()
 		if file_under_cursor and isdir(file_under_cursor):
